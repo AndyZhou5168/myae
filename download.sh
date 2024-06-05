201a6d170102783298a7539224cae88b70558b1e
AndyZhou5168 <66496837@qq.com>
2024-06-05 11:33:25 +0800

    优化download.sh

--- download.sh
+++ download.sh
@@ -6,31 +6,31 @@ download(){
  wget -N --continue -P./binaries/ $*
 }
 
-echo "Downloading binaries..."
+echo -e "Downloading binaries...\n"
 
-echo "Downloading kernel 2.6 (MIPS)..."
+echo -e "Downloading kernel 2.6 (MIPS)...\n"
 download https://github.com/pr0v3rbs/FirmAE_kernel-v2.6/releases/download/v1.0/vmlinux.mipsel.2
 download https://github.com/pr0v3rbs/FirmAE_kernel-v2.6/releases/download/v1.0/vmlinux.mipseb.2
 
-echo "Downloading kernel 4.1 (MIPS)..."
+echo -e "Downloading kernel 4.1 (MIPS)...\n"
 download https://github.com/pr0v3rbs/FirmAE_kernel-v4.1/releases/download/v1.0/vmlinux.mipsel.4
 download https://github.com/pr0v3rbs/FirmAE_kernel-v4.1/releases/download/v1.0/vmlinux.mipseb.4
 
-echo "Downloading kernel 4.1 (ARM)..."
+echo -e "Downloading kernel 4.1 (ARM)...\n"
 download https://github.com/pr0v3rbs/FirmAE_kernel-v4.1/releases/download/v1.0/zImage.armel
 download https://github.com/pr0v3rbs/FirmAE_kernel-v4.1/releases/download/v1.0/vmlinux.armel
 
-echo "Downloading busybox..."
+echo -e "Downloading busybox...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/busybox.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/busybox.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/busybox.mipsel
 
-echo "Downloading console..."
+echo -e "Downloading console...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/console.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/console.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/console.mipsel
 
-echo "Downloading libnvram..."
+echo -e "Downloading libnvram...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram.so.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram.so.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram.so.mipsel
@@ -38,17 +38,17 @@ download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram_ioct
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram_ioctl.so.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/libnvram_ioctl.so.mipsel
 
-echo "Downloading gdb..."
+echo -e "Downloading gdb...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdb.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdb.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdb.mipsel
 
-echo "Downloading gdbserver..."
+echo -e "Downloading gdbserver...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdbserver.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdbserver.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/gdbserver.mipsel
 
-echo "Downloading strace..."
+echo -e "Downloading strace...\n"
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/strace.armel
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/strace.mipseb
 download https://github.com/pr0v3rbs/FirmAE/releases/download/v1.0/strace.mipsel
